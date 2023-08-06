4c8054be9c21a010b8be4dfcc5ab2fbc68ed1e62
Bryce Coakley <b.coakley@hotmail.com>
2023-08-06 10:24:36 -0500

    Added function that instantiates a canvas to draw on (16x16 grid).

---
+++ script.js
@@ -0,0 +1,21 @@
+
+const canvas = document.querySelector('.canvas');
+createCanvas();
+
+class sketchCell {
+
+}
+
+function createCanvas() {
+    for(let i = 0; i < 16; i++) {
+            const newDiv = document.createElement('div');
+            newDiv.className = 'rowDiv';
+            canvas.appendChild(newDiv);
+
+        for(let i = 0; i < 16; i++) {
+                const newSketchCell = canvas.appendChild(document.createElement('div'));
+                newSketchCell.className = 'sketchCell';
+                newDiv.appendChild(newSketchCell);
+        }
+    }
+}
