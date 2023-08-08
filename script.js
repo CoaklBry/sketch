c06b67be4032e7fc2d25cf16601957f04d1b9b0c
Bryce Coakley <b.coakley@hotmail.com>
2023-08-08 09:45:04 -0500

    Added changing classes that recolors cells as they are mouseover'd. TODO: Add a reset button, add ability to customize the number of cells.

--- script.js
+++ script.js
@@ -2,23 +2,22 @@
 const canvas = document.querySelector('.canvas');
 createCanvas();
 
-class sketchCell {
-
-}
-
 function createCanvas() {
     for(let i = 0; i < 16; i++) {
             const newDiv = document.createElement('div');
+
             newDiv.className = 'rowDiv';
             canvas.appendChild(newDiv);
 
         for(let i = 0; i < 16; i++) {
                 const newSketchCell = canvas.appendChild(document.createElement('div'));
                 newSketchCell.className = 'sketchCell';
-                newSketchCell.addEventListener('click', () => {
-                    newSketchCell.setAttribute('style', 'background-color: black;');
-                });
+                newSketchCell.addEventListener('mouseover', () => (switchDivClass(newSketchCell)));
                 newDiv.appendChild(newSketchCell);
         }
     }
+}
+
+function switchDivClass(cell) {
+    cell.className = 'coloredCell';
 }
