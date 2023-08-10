af1fb518f3a1dc5b7fe18038bb11e98924b0c34c
Bryce Coakley <b.coakley@hotmail.com>
2023-08-10 11:42:07 -0500

    Added clearing canvas functionality

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 
 const canvas = document.querySelector('.canvas');
+const numRows = 
 createCanvas();
+const createBtn = document.querySelector('.createBtn');
+createBtn.addEventListener('click', () => {
+    if (canvas.lastChild) {clearCanvas(canvas);}
+    createCanvas(canvas);
+})
 
 function createCanvas() {
     for(let i = 0; i < 16; i++) {
@@ -20,4 +26,12 @@ function createCanvas() {
 
 function switchDivClass(cell) {
     cell.className = 'coloredCell';
+}
+
+function clearCanvas(parent) {
+    while (parent.lastChild) {
+        parent.removeChildren(parent.lastChild);
+    };
+
+    createCanvas();
 }
