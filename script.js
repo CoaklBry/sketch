917ecd3bbea7bcb5aad949164ad1ca6c1d68af61
Bryce Coakley <b.coakley@hotmail.com>
2023-08-13 13:49:30 -0500

    Fixed rows not instantiating correctly. Project seems to work as intended TODO: Add better styling and add drag and drop capabilities.

--- script.js
+++ script.js
@@ -1,36 +1,38 @@
 
 const content = document.querySelector('.content');
 const canvas = document.querySelector('.canvas');
+const canvasWidth = canvas.clientWidth + 2;
+const canvasHeight = canvas.clientHeight + 2;
 const cellSlider = document.querySelector('.numCellsSlider');
 const numCellsOutput = document.querySelector('#numCellsOutput');
 numCellsOutput.innerHTML = cellSlider.value;
 cellSlider.oninput = function() {numCellsOutput.innerHTML = this.value};
-createCanvas();
+createCanvas(16);
 const createBtn = document.querySelector('.createBtn');
 createBtn.addEventListener('click', () => {
     if (canvas.lastChild) {clearCanvas(canvas);}
-    createCanvas();
+    createCanvas(cellSlider.value);
 });
 
-function createCanvas() {
-    for(let i = 0; i < 16; i++) {
+function createCanvas(numCells) {
+    for(let i = 0; i < numCells; i++) {
             const newDiv = document.createElement('div');
-
             newDiv.className = 'rowDiv';
             canvas.appendChild(newDiv);
 
-        for(let j = 0; j < 16; j++) {
-                //const newSketchCell = canvas.appendChild(document.createElement('div'));
+        for(let j = 0; j < numCells; j++) {
                 const newSketchCell = document.createElement('div');
                 newSketchCell.className = 'sketchCell';
-                newSketchCell.addEventListener('mouseover', () => (switchDivClass(newSketchCell)));
+                newSketchCell.addEventListener('mouseover', (e) => {
+                    (switchDivClass(newSketchCell))
+                });
                 newDiv.appendChild(newSketchCell);
         }
     }
 }
 
 function switchDivClass(cell) {
-    cell.className = 'coloredCell';
+    cell.classList.add('coloredCell');
 }
 
 function clearCanvas(parent) {
